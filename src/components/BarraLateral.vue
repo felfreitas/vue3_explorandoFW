ed3638f4c10e1111a2e735eba6152d6638bbe871
Felipe Freitas <soufreitas.felipe@gmail.com>
2023-08-29 18:33:27 -0300

    Criação e fatoração compenentes Formulario, cronometro e temporizador

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,35 @@
+<template>
+   <header>
+    <h1>
+        <img src="../assets/logo.png" alt="Logo do Vue: Ela é a letra V com dentro de um círculo">
+    </h1>
+   </header>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name:'BarraLateral'
+});
+</script>
+
+<style scoped>
+/* fica todo o stilo do componente */
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+/* mobile */
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+
+</style>
+
---
+++ src/components/Cronometro.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <strong>
+      {{ tempoDecorrido }}
+    </strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Cronometro-Nome",
+  props: {
+    //tudo que vamos reeceber do pai
+    tempoEmSegundos: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    //serve p uma informação dinamica: monitora uma informação
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().slice(11, 19);
+    },
+  },
+});
+</script>
+
+<style></style>
