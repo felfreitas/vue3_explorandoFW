5d2969908781ec81e948f1d7602bddcbdb7f134f
Felipe Freitas <soufreitas.felipe@gmail.com>
2023-09-01 13:01:55 -0300

    inserindo um metodo setup() dentro do formulario para pegar os projetos

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div
-        class="column is-8"
-        role="form"
-        aria-label="Formulario para criação de uma nova tarefa"
-      >
-        <input
-          type="text"
-          class="input"
-          placeholder="Qual tarefa você deseja iniciar?"
-          v-model="descricao"
-        />
+      <div class="column is-5" role="form" aria-label="Formulario para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao" />
       </div>
 
       <div class="column">
@@ -22,8 +13,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Temporizador from "./Temporizador.vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
 
 export default defineComponent({
   name: "Formulario-Nome",
@@ -34,8 +27,8 @@ export default defineComponent({
   },
   data() {
     return {
-      descricao: "",
-      dataInicial: new Date(),
+      descricao: ""
+      // dataInicial: new Date(),
     };
   },
   methods: {
@@ -47,11 +40,18 @@ export default defineComponent({
       this.descricao = "";
     },
   },
+  setup() {
+    //setup serve para buscar os dados da Store. Passando uma key e retornando computed!
+    const store = useStore(key)
+    return {
+      projetos: computed(() => store.state.projetos)
+    }
+  }
 });
 </script>
 
 <style>
-.formulario{
+.formulario {
   color: var(--texto-primario);
   background-color: var(--bg-primario);
 }
